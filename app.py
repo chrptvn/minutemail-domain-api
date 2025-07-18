4d3cac5b798a9149f6bc67f804bec328e9991f40
cacpot <christian.potvin@vanderlande.com>
2025-07-17 23:19:34 -0400

    wip

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import json
+import random
+import string
 
 import requests
 from fastapi import FastAPI, HTTPException, Header, status
 import redis
 import os
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel
 from jose import jwt, JWTError
 import dns.resolver
 
-class ClaimedDomain(BaseModel):
-    domain: str
 
+class DomainClaim(BaseModel):
+    domain: str
+    mailbox_ttl: Optional[int] = 3600
 
 app = FastAPI()
 
 redis_host = os.getenv("REDIS_HOST", "127.0.0.1")
-file_service_url = os.getenv("FILE_SERVICE_URL", "http://tempfile-servlet.minutemail.svc.cluster.local:8080")
 redis_port = int(os.getenv("REDIS_PORT", "6379"))
 redis_db = int(os.getenv("REDIS_DB", "2"))
 
@@ -73,38 +75,48 @@ def get_claims(auth_header: str) -> dict:
 
     return claims
 
-@app.get("/v1/domains/verify", summary="Verify a claimed domain")
-def verify_domain(
-    domain: str,
-    auth_header: str = Header(None, alias="Authorization")
+def random_string(length: int):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choices(characters, k=length))
+
+def verify_mx(
+    domain: str
 ):
     domain_name = domain.lower()
-    valid_mx_records = ['mail.minutemail.co', 'smtp1.minutemail.co']
-    get_claims(auth_header)
+    valid_mx_records = ['smtp1.minutemail.co']
 
     try:
         records = dns.resolver.resolve(domain_name, 'MX')
-        mx_hosts = [r.exchange.to_text(omit_final_dot=True).lower() for r in records]
+        mx_hosts = [record.exchange.to_text(omit_final_dot=True).lower() for record in records]
 
-        return {"valid": all(mx in valid_mx_records for mx in mx_hosts), "mx_records": mx_hosts}
+        return all(mx in valid_mx_records for mx in mx_hosts)
     except Exception as e:
         print(f"Failed to get MX records for {domain_name}: {e}")
         return {"valid": False, "error": str(e) if str(e) else "Failed to resolve MX records"}
 
+def verify_txt(
+        domain: str,
+        txt_verification: str,
+):
+    domain_name = domain.lower()
+    try:
+        records = dns.resolver.resolve(domain_name, 'TXT')
+        for r in records:
+            if txt_verification in r.to_text():
+                return True
+    except Exception as e:
+        print(f"Failed to get TXT records for {domain_name}: {e}")
+
+    return False
 
 @app.post("/v1/domains", summary="Claim a new domain")
 async def claim_domain(
-    req: ClaimedDomain,
+    domainClaim: DomainClaim,
     auth_header: str = Header(None, alias="Authorization")
 ):
     user_id = await get_user_id(auth_header)
-    domain_name = req.domain.lower()
-
-    if not verify_domain(domain_name, auth_header)["valid"]:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Domain '{domain_name}' is not valid or does not have the required MX records."
-        )
+    domain_name = domainClaim.domain.lower()
+    txt_verification = f"minutemail-{random_string(16)}"
 
     domain_key = f"domain:{domain_name}"
     user_domains_set_key = f"user:{user_id}:domains"
@@ -112,22 +124,29 @@ async def claim_domain(
     claimed_by_user_id = redis_domains.get(domain_key)
 
     if claimed_by_user_id:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=f"Domain '{domain_name}' is already claimed."
-        )
+        if claimed_by_user_id == user_id:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"Domain '{domain_name}' is already claimed."
+            )
 
     try:
+        claimed_domain = {
+            "name": domain_name,
+            "verification": txt_verification,
+            "mailbox_ttl": domainClaim.mailbox_ttl
+        }
+
         pipe = redis_domains.pipeline()
 
-        pipe.set(domain_key, user_id)
-        pipe.sadd(user_domains_set_key, json.dumps({
-            "domain": domain_name
-        }))
+        await pipe.set(domain_key, user_id)
+        await pipe.sadd(user_domains_set_key, json.dumps(claimed_domain))
 
         pipe.execute()
 
-        return {"message": f"Domain '{domain_name}' claimed successfully by user '{user_id}'."}
+        claimed_domain["mx_valid"] = verify_mx(domain_name)
+        claimed_domain["txt_valid"] = False
+        return claimed_domain
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -135,7 +154,7 @@ async def claim_domain(
         )
 
 
-@app.get("/v1/domains", response_model=List[ClaimedDomain], summary="Fetch all domains claimed by the current user")
+@app.get("/v1/domains", response_model=List[DomainClaim], summary="Fetch all domains claimed by the current user")
 async def fetch_domains(
     auth_header: str = Header(None, alias="Authorization")
 ):
@@ -143,8 +162,12 @@ async def fetch_domains(
     user_domains_set_key = f"user:{user_id}:domains"
 
     try:
-        claimed_domains = redis_domains.smembers(user_domains_set_key)
-        return [json.loads(d) for d in claimed_domains]
+        claimed_domains = []
+        for domain in redis_domains.smembers(user_domains_set_key):
+            claimed_domain = json.loads(domain)
+            claimed_domain["mx_valid"] = verify_mx(claimed_domain["name"])
+            claimed_domain["txt_valid"] = verify_txt(claimed_domain["name"], claimed_domain["verification"])
+            claimed_domains.append(claimed_domain)
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
